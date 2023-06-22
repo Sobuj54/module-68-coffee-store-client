d416b3655f177572b1878c1533f6723f43a4f5cc
soubj <sobujahmed124@gmail.com>
2023-06-22 21:40:49 +0600

    added two form fields

--- src/components/AddCoffee.jsx
+++ src/components/AddCoffee.jsx
@@ -1,5 +1,35 @@
 const AddCoffee = () => {
-  return <div> Add a Coffee.</div>;
+  return (
+    <div>
+      Add a Coffee.
+      <form>
+        <div className="flex">
+          <div className="form-control">
+            <label className="label"></label>
+            <label className="input-group">
+              <span className="label-text">Name</span>
+              <input
+                type="text"
+                placeholder="Coffee Name"
+                className="input input-bordered"
+              />
+            </label>
+          </div>
+          <div className="form-control">
+            <label className="label"></label>
+            <label className="input-group">
+              <span className="label-text">Quantity</span>
+              <input
+                type="text"
+                placeholder="Coffee Quantity"
+                className="input input-bordered"
+              />
+            </label>
+          </div>
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default AddCoffee;
